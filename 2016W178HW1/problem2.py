2ec660ec7fcc33e7461fe7ab43ce042176fa46db
Chuan Li <lichuan0620@gmail.com>
2017-07-15 19:28:58 -0700

    problem 2b

--- 2016W178HW1/problem2.py
+++ 2016W178HW1/problem2.py
@@ -27,3 +27,27 @@ for i, k in enumerate([1, 5, 10, 50]):
 
 plt.show()
 plt.close(1)
+
+# problem 2(b)
+K = [1, 2, 5, 10, 50, 100, 200]
+errTrain = []
+errTest = []
+lenTestY = testY.shape[0]
+lenTrainY = trainY.shape[0]
+
+for i, k in enumerate(K):
+    learner = mltools.knn.knnClassify()
+    learner.train(trainX[:, 0:2], trainY, k)
+    errTrain.append(1 - float(numpy.sum(learner.predict(trainX[:, 0:2]) == trainY))/float(lenTrainY))
+    errTest.append(1 - float(numpy.sum(learner.predict(testX[:, 0:2]) == testY))/float(lenTestY))
+
+plt.figure(2, (10, 6))
+plt.semilogx(K, errTrain, 'rh-', label="train error")
+plt.semilogx(K, errTest, 'gH-', label="test error")
+plt.legend()
+plt.xlabel('K')
+plt.ylabel('%error')
+plt.title('Iris KNN: percentage error relative to K')
+plt.grid(1)
+plt.show()
+plt.close(2)
